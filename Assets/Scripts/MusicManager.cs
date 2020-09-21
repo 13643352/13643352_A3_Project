ec6cdc5164e3032a833860b7d6a38d0321a88b4c
13643352 <13643352@student.uts.edu.au>
2020-09-21 16:21:37 +1000

    Music Input

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource startMusic;
+    public AudioSource BGM;
+    public float BGMDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BGMDelay = BGM.time;
+
+        BGM.Play((ulong)BGMDelay);
+    }
+}
